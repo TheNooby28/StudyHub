d98cc5740a88736c44a6071196069ae820a706cc
TheNooby28 <jayjadamoski@gmail.com>
2026-01-28 18:38:45 -0500

    Started the server, added a BUNCH to index.html

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import supabase from 'db.js';
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(cors());
+app.use(express.json());
+
